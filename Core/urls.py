cf8409aee1abcc5efa227ac868dd55d6447c4ed6
wokuma <wokumateshome13@gmail.com>
2024-08-31 16:16:06 +0300

    commit

--- Core/urls.py
+++ Core/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [ 
     path('',views.index,name='index'), 
-    path('profile/',views.profile,name='profile')  
+    path('profile/',views.profile,name='profile'),
+    path('activities',views.activities,name='activities'),
+     path('evaluation',views.evaluation,name='evaluation')
 ]
