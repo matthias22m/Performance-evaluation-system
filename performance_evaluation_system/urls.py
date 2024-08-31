29000e995df9f07be842c8ecc26fcdb6db7eed39
wokuma <wokumateshome13@gmail.com>
2024-08-31 08:33:16 +0300

    commit

--- performance_evaluation_system/urls.py
+++ performance_evaluation_system/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Core.urls')),
+    path('cors/', include('Core.urls')),
     path('users/', include('Users.urls')),
     
 ]
