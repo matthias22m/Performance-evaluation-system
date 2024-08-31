8c431ffe75f463dc880fd7650e283f6956d0e9a0
wokuma <wokumateshome13@gmail.com>
2024-08-31 08:29:15 +0300

    commit

--- Core/urls.py
+++ Core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [    
+urlpatterns = [ 
+    path('',views.index,name='index'), 
+    path('profile/',views.profile,name='profile')  
 ]
