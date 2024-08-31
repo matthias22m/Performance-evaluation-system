fac8b150eda9308140b8c8dfd1521058215a0321
Matthias Mulugeta <matthias22m@gmail.com>
2024-08-31 10:44:25 +0300

    added login view and page

--- Users/urls.py
+++ Users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
+from .forms import CustomLoginForm
 from . import views
 
 urlpatterns = [    
+    path("login/", auth_views.LoginView.as_view(template_name= 'users/login.html', authentication_form=CustomLoginForm), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),              
 ]
