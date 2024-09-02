e503466226edae4f507bc8d0edd72fa6f2387107
Hakimii12 <abdulhakimago259@gmail.com>
2024-09-03 00:08:36 +0300

    create an home.html file for employee

--- Users/views.py
+++ Users/views.py
@@ -21,14 +21,14 @@ def EmployeeView(request):
     user = request.user
     target_employee = Employee.objects.get(id = user.id)
     employee_image = Profile.objects.get(id = user.id)
-    # employee_group = Group.objects.get(id = user.id)
-    # character_evaluation_employee = CharacterEvaluation.objects.get(id = user.id)
-    # total_evaluation = character_evaluation_employee.behavior_one + character_evaluation_employee.behavior_two + character_evaluation_employee.behavior_three + character_evaluation_employee.behavior_four + character_evaluation_employee.behavior_five + character_evaluation_employee.behavior_six
+    employee_group = Group.objects.get(id = user.id)
+    character_evaluation_employee = CharacterEvaluation.objects.get(id = user.id)
+    total_evaluation = character_evaluation_employee.behavior_one + character_evaluation_employee.behavior_two + character_evaluation_employee.behavior_three + character_evaluation_employee.behavior_four + character_evaluation_employee.behavior_five + character_evaluation_employee.behavior_six
     context = {
         "target_employee": target_employee,
         "employee_image":employee_image,
-        # "employee_group":employee_group,
-        # "total_evaluation":total_evaluation
+        "employee_group":employee_group,
+        "total_evaluation":total_evaluation
     }
     return render(request, 'Users/homePage.html', context)
 
