c56e39dae0b61c4962eedbf7e8d55bf665d6c262
Milkessa <milkesause@gmail.com>
2024-09-02 11:33:16 +0300

    employee list and detail

--- Core/urls.py
+++ Core/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path('subactivity_create/', views.subactivity_create, name='create_subactivity'),
-    path('subactivity_list/', views.subactivity_list, name='subactivity_list')
+    path('subactivity_list/', views.subactivity_list, name='subactivity_list'),
+    path('', views.employee_list, name='employee_list'),
+    path('employee/<int:id>/', views.employee_detail, name='employee_detail')
 ]
+
+
+
