1980602c2715e56083116f64b9fb859f106167fa
eyualpha <eyuashenafi533@gmail.com>
2024-09-02 17:10:35 +0300

    created employee view

--- Users/views.py
+++ Users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import EmployeeRegisterForm
 from django.contrib.auth.decorators import login_required
+
 from .models import Employee, Profile, Group
 from Core.models import CharacterEvaluation
 
