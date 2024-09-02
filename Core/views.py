847e9a076bd877c5db884dfb1cd95da1da01ac8b
Fenet-damena <fenetdamena74@gmail.com>
2024-09-02 01:06:21 -1200

    navbar

--- Core/views.py
+++ Core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'core/index.html')
+
+def activities(request):
+    return render(request, 'core/activities.html')
+
+def employee(request):
+    return render(request, 'core/employee.html')
+
+def evaluation(request):
+    return render(request, 'core/evaluation.html')
+
+def logout_view(request):
+    return render(request, 'core/logout.html')  # Redirect or render a logout page
