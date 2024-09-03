829f6baffee7f84800dacb88b152d12576b367ec
Matthias Mulugeta <matthias22m@gmail.com>
2024-09-03 23:25:00 +0300

    added signals for updating unit leaders position

---
+++ Core/signals.py
@@ -0,0 +1,39 @@
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+from .models import Unit, WorkUnit
+
+@receiver(post_save, sender=Unit)
+def update_unit_leader_position(sender, instance, created, **kwargs):
+    if instance.unit_leader:
+        instance.unit_leader.position = instance.position_id
+        instance.unit_leader.save()
+
+@receiver(pre_save, sender=Unit)
+def handle_unit_leader_change(sender, instance, **kwargs):
+    if instance.pk:
+        old_unit = Unit.objects.get(pk=instance.pk)
+        if old_unit.unit_leader and old_unit.unit_leader != instance.unit_leader:
+            old_unit.unit_leader.position = None
+            old_unit.unit_leader.save()
+
+        if instance.unit_leader and old_unit.unit_leader != instance.unit_leader:
+            instance.unit_leader.position = instance.position_id
+            instance.unit_leader.save()
+
+@receiver(post_save, sender=WorkUnit)
+def update_workunit_manager_position(sender, instance, created, **kwargs):
+    if instance.manager:
+        instance.manager.position = instance.position_id
+        instance.manager.save()
+
+@receiver(pre_save, sender=WorkUnit)
+def handle_workunit_manager_change(sender, instance, **kwargs):
+    if instance.pk:
+        old_workunit = WorkUnit.objects.get(pk=instance.pk)
+        if old_workunit.manager and old_workunit.manager != instance.manager:
+            old_workunit.manager.position = None
+            old_workunit.manager.save()
+
+        if instance.manager and old_workunit.manager != instance.manager:
+            instance.manager.position = instance.position_id
+            instance.manager.save()
