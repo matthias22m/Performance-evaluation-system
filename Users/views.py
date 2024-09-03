af746f5d96157be5f83e021c0738133475adbfc7
eyualpha <eyuashenafi533@gmail.com>
2024-09-03 15:11:48 +0300

    implemented view for recent_activities of the employee profile page

--- Users/views.py
+++ Users/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .forms import EmployeeRegisterForm
 from django.contrib.auth.decorators import login_required
 from .models import Employee, Profile, Group
-from Core.models import CharacterEvaluation
+from Core.models import CharacterEvaluation, SubActivity
+
+
 def register(request):
     if request.method == 'POST':
         form = EmployeeRegisterForm(request.POST)
@@ -19,10 +21,10 @@ def register(request):
 
 def EmployeeView(request):
     user = request.user
-    target_employee = Employee.objects.get(id = user.id)
-    employee_image = Profile.objects.get(id = user.id)
+    target_employee = get_object_or_404(Employee, id=user.id)
+    employee_image = Profile.objects.filter(id = user.id).first()
+    recent_activities = SubActivity.objects.filter(employee=target_employee).values()
     employee_group = target_employee.group.all().first()
-        
 
     evaluations = CharacterEvaluation.objects.get(id = user.id)
     total_evaluation = evaluations.behavior_one + evaluations.behavior_two + evaluations.behavior_three + evaluations.behavior_four + evaluations.behavior_five + evaluations.behavior_six
@@ -31,7 +33,8 @@ def EmployeeView(request):
         "employee_image":employee_image,
         "employee_group":employee_group,
         "total_evaluation":total_evaluation,
-        "employees":employee_group.employee.all()
+        "employees":employee_group.employee.all(),
+        "recent_activities":recent_activities
     }
     return render(request, 'Users/homePage.html', context)
 
