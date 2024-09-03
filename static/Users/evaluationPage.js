e787eb0ccc0bc1dcd0d652415210d91a61819089
Hakimii12 <abdulhakimago259@gmail.com>
2024-09-03 09:42:29 +0300

    frontpage for employee views

--- static/Users/evaluationPage.js
+++ static/Users/evaluationPage.js
@@ -4,11 +4,10 @@ function showitems(){
  function hideItmes(){
      document.getElementById("navbar").style.display="none";
  }
- const windowWidth = window.innerWidth; 
  window.addEventListener('resize', ()=>{
-    const windowWidth = window.innerWidth; 
-    if(windowWidth>800){
-        document.getElementById("navbar").style.display="none";
-    }
-
-});
+     const windowWidth = window.innerWidth; 
+     if(windowWidth>800){
+         document.getElementById("navbar").style.display="none";
+     }
+ 
+ });
