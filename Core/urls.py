01033eeb67bb2e8045839c7f7f9a2c267c237162
da-nn-yy <db7394928@gmail.com>
2024-09-06 11:00:59 +0200

    modified the urls

--- Core/urls.py
+++ Core/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('units/', views.units, name='units'),
     path('plan/', views.plan, name='plan'),
-    # path('logout/', views.logout_view, name='logout'),
+    # path('logout/', views.logout, name='logout'),
 ]
