280dd5dc1d273c5f7d7056acb91afbdc1dd88213
da-nn-yy <db7394928@gmail.com>
2024-09-06 09:15:20 +0200

    deleted templets

--- Core/urls.py
+++ Core/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [ 
-     
+urlpatterns = [
+
     path('profile/',views.profile,name='profile'),
     path('activities',views.activities,name='activities'),
     path('evaluation',views.evaluation,name='evaluation'),
-    path('char_evaluation',views.char_evaluation,name='char_evaluation'),
-    path('leader',views.leader_page,name='leader_page'),
+    # path('char_evaluation',views.char_evaluation,name='char_evaluation'),
+    # path('leader',views.leader_page,name='leader_page'),
      path('evaluation/',views.evaluation,name='evaluation')
 ]
