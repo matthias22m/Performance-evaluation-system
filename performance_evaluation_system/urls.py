d9305be34efd0f1278311b7cc98615be0ddac2ed
da-nn-yy <db7394928@gmail.com>
2024-09-06 10:01:15 +0200

    modified the main urls

--- performance_evaluation_system/urls.py
+++ performance_evaluation_system/urls.py
@@ -5,11 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cors/', include('Core.urls')),
+    path('', include('Core.urls')),
     path('users/', include('Users.urls')),
-    
+
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
-
